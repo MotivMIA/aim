b99e3070766eb81b51d979eeac93a4b0d62cbeab
Nathan <nathanwilliams1149@gmail.com>
2025-04-25 23:19:25 -0700

    update

--- create-test-wallet.py
+++
@@ -1,8 +0,0 @@
-from xrpl.clients import JsonRpcClient
-from xrpl.wallet import generate_faucet_wallet
-
-client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
-test_wallet = generate_faucet_wallet(client, debug=True)
-print(f"Test Wallet Address: {test_wallet.classic_address}")
-print(f"Test Wallet Seed: {test_wallet.seed}")
-print(f"Funded with 10,000 testnet XRP")
